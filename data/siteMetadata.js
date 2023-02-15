846cb5326c32ea1731841fa06bb94d2a406638cf
Lebyy <45114019+Lebyy@users.noreply.github.com>
2023-02-15 22:32:08 +0530

    ⚒️Removing simpleAnalytics

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -21,7 +21,7 @@ const siteMetadata = {
     // content security policy in the `next.config.js` file.
     // supports plausible, simpleAnalytics, umami or googleAnalytics
     plausibleDataDomain: 'blog.lebyy.me', // e.g. tailwind-nextjs-starter-blog.vercel.app
-    simpleAnalytics: true, // true or false
+    simpleAnalytics: false, // true or false
     umamiWebsiteId: '', // e.g. 123e4567-e89b-12d3-a456-426614174000
     googleAnalyticsId: '', // e.g. UA-000000-2 or G-XXXXXXX
     posthogAnalyticsId: '', // posthog.init e.g. phc_5yXvArzvRdqtZIsHkEm3Fkkhm3d0bEYUXCaFISzqPSQ
